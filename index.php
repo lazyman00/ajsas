d686fb9cbd70900000378fe56e762860381feb8f
lazyman00 <sky_of_light@hotmail.com>
2019-11-20 15:23:59 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>วารสารวิชาการวิทยาศาสตร์และวิทยาศาสตร์ประยุกต์</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <style type="text/css">
+        .navbar-default .navbar-nav>li>a {
+            color: #861c20;
+        }
+        .navbar-default .navbar-nav>li>a:hover {
+            color: #fff;
+            background-color:#861c20;
+            border-radius: 50px;
+        }
+        .box {
+            box-shadow: 3px 5px 17px -2px #cecece;
+            height: 90px;
+        }
+        .x5 ul {
+            text-align: right;
+            padding: 11px;
+        }
+        .error{
+            color: red;
+        }
+
+    </style>
+</head>
+<body>
+
+    <div class="container-fluid" >
+       <div class="row" style="background-color:#008000;height: 100px;">
+          <div class="container">
+            <div class="row">
+              <div class="x1 text-center" >
+					<!-- <p style="color: #fff; padding-top: 20px; font-size: 30px; margin-top: -16px;">ระบบกิจกรรมชุมนุมออนไลน์</p>
+                       <p style="color: #fff; font-size: 20px; margin-top: -15px;"> โรงเรียนแม่จริม อำเภอแม่จริม จังหวัดน่าน</p> -->
+                   </div>
+               </div>
+           </div>
+       </div>
+
+       <div class="row box" >
+         <div class="container re">
+            <div class="row">
+                <nav class="navbar navbar-default">			      
+                  <div class="row">
+                    <div  class="col-md-5 ">
+                        <img src="img/banner.png" style="position: static;margin-top: -90px;margin-left: -110px;width: 680px;height: 140px;">
+                    </div>
+                    <div  class="col-md-7 " style="align:right">
+                        <button type="button" class="btn btn-outline-success" data-toggle="modal" data-target=".bd-example-modal-sm" style="width: 180px;margin-left: 249px;">เข้าสู่ระบบวารสาร</button>
+                        <button type="button" class="btn btn-outline-success" style="width: 190px;margin-left: 10px;">สมัครสมาชิกส่งบทความ</button>
+                    </div>
+                </div><br><br><br>           
+            </nav>
+        </div>
+    </div>
+    <div class="container">
+        <div class="col-md-12">
+            <h3 style="font-family: prompt;font-size: 28px;font-weight: 500;color: #555;text-align:center">วารสารวิชาการวิทยาศาสตร์และวิทยาศาสตร์ประยุกต์ </h3>
+        </div>
+        <div class="col-md-12">
+            <h3 style="font-family: prompt;font-size: 18px;font-weight: 500;color: #555;text-align:center">คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏอุตรดิตถ์ </h3>
+        </div><br>
+        <div class="col-md-12">
+            <h3 style="font-family: prompt;font-size: 28px;font-weight: 500;color: #006400;text-align:center">Academic Journal of Science and Applied Science  </h3>
+        </div>
+        <div class="col-md-12">
+            <h3 style="font-family: prompt;font-size: 18px;font-weight: 500;color: #555;text-align:center">Faculty of Science and Technology, Uttaradit Rajabhat University </h3>
+        </div><br><br>
+        <div class="col-md-12">
+            <h3 style="font-family: prompt;font-size: 18px;font-weight: 500;color: #555;text-align:center">เพื่อเผยแพร่ความรู้เชิงวิชาการ หรือการวิจัย ด้านวิทยาศาสตร์และวิทยาศาสตร์ประยุกต์ ได้แก่ วิทยาศาสตร์ที่เกิดขึ้นในเหตุการณ์ปัจจุบัน วิทยาศาสตร์บริสุทธิ์ วิทยาศาสตร์กายภาพ วิทยาศาสตร์และเทคโนโลยี วิทยาศาสตร์ สิ่งแวดล้อม วิทยาศาสตร์สุขภาพ และสาขาที่เกี่ยวข้อง </h3>
+        </div><br><br>
+        <div class="col-md-12">
+            <h3 style="font-family: prompt;font-size: 28px;font-weight: 500;color: #555;text-align:center">กำหนดการจัดทำวารสารแต่ละปี</h3>
+        </div>
+        <div class="col-md-12">
+            <h3 style="font-family: prompt;font-size: 18px;font-weight: 500;color: #555;text-align:center">ฉบับที่ 1 มกราคม – มิถุนายน</h3>
+        </div>
+    </div>
+
+    <div class="container">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col" style=" width:10%;text-align:center">ลำดับ</th> 
+                    <th scope="col" style=" width:60%;text-align:center">กิจกรรม</th>
+                    <th scope="col" style=" width:30%;text-align:center">ช่วงระยะเวลา</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="text-align:center">1</td>
+                    <td>ประชาสัมพันธ์</td>
+                    <td style="text-align:center">ม.ค. - ก.พ.</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center">2</td>
+                    <td>รับข้อเสนอบทความ</td>
+                    <td style="text-align:center">ก.พ. - มี.ค.</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center">3</td>
+                    <td>ส่งผู้ทรงคุณวุฒิตรวจสอบวิชาการ</td>
+                    <td style="text-align:center">ก.พ. - เม.ย.</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center">4</td>
+                    <td>ส่งคืนเพื่อปรับแก้ไขตามข้อเสนอแนะ</td>
+                    <td style="text-align:center">เม.ย - พ.ค.</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center">5</td>
+                    <td>ตรวจสอบและพิมพ์วารสาร</td>
+                    <td style="text-align:center">มิ.ย.</td>
+                </tr>
+            </tbody>
+        </table><br>
+    </div>
+
+    <br><br><br><br><br><br>
+    <div class="container">
+        <div class="col-md-12">
+            <h3 style="font-family: prompt;font-size: 18px;font-weight: 500;color: #555;text-align:center">ฉบับที่ 2 กรกฎาคม – ธันวาคม</h3>
+        </div>
+    </div>
+
+    <div class="container">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col" style=" width:10%;text-align:center">ลำดับ</th> 
+                    <th scope="col" style=" width:60%;text-align:center">กิจกรรม</th>
+                    <th scope="col" style=" width:30%;text-align:center">ช่วงระยะเวลา</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="text-align:center">1</td>
+                    <td>ประชาสัมพันธ์</td>
+                    <td style="text-align:center">ก.ค. - ส.ค.</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center">2</td>
+                    <td>รับข้อเสนอบทความ</td>
+                    <td style="text-align:center">ส.ค. - ก.ย.</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center">3</td>
+                    <td>ส่งผู้ทรงคุณวุฒิตรวจสอบวิชาการ</td>
+                    <td style="text-align:center">ส.ค. - พ.ย.</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center">4</td>
+                    <td>ส่งคืนเพื่อปรับแก้ไขตามข้อเสนอแนะ</td>
+                    <td style="text-align:center">ต.ค. - พ.ย.</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center">5</td>
+                    <td>ตรวจสอบและพิมพ์วารสาร</td>
+                    <td style="text-align:center">ธ.ค.</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+</div>
+
+
+<div class="modal fade bd-example-modal-sm" id="loginform" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">เข้าสู่ระบบ</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form class="login" id="loginA" name="loginA" method="post" >
+                <div class="modal-body">
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">อีเมล</label>
+                        <input type="text" name="user" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="อีเมล">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">รหัสผ่าน</label>
+                        <input type="password" name="pass" class="form-control" id="exampleInputPassword1" placeholder="รหัสผ่าน">
+                    </div>
+
+                </div>
+                <div class="modal-footer" style="justify-content: center;">
+                    <button type="submit" class="btn btn-primary">ตกลง</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal fade bd-example-modal-sm" id="msg" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="alert alert-warning alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+            <strong>ข้อความ!</strong> <span id="txt">ชื่อผู้ใช้หรือรหัสผ่านผิดพลาด กรุณากรอกข้อมูลอีกครั้ง!!</span>
+        </div>
+    </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script type="text/javascript" src="jquery-validation-1.19.0/dist/jquery.validate.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        // $('#msg1').modal('show');
+
+        $( "#loginA" ).validate( {
+            rules: {
+                user: "required",
+                pass: "required"
+
+            },
+            messages: {
+                user: "*กรุณากรอกข้อมูล",
+                pass: "*กรุณาเลือกข้อมูล"
+
+            },
+            errorElement: "em",
+            errorPlacement: function ( error, element ) {
+                error.addClass( "help-block" );
+
+                if ( element.prop( "type" ) === "checkbox" ) {
+                    error.insertAfter( element.parent( "label" ) );
+                } else {
+                    error.insertAfter( element );
+                }
+            },
+            highlight: function ( element, errorClass, validClass ) {
+                $( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
+            },
+            unhighlight: function (element, errorClass, validClass) {
+                $( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
+            },
+            submitHandler: function(e) {
+
+                $('#loginform').modal('hide');
+                setTimeout(function(){ 
+                    $.post('login/sql_login.php',$('#loginA').serialize(),function(response){
+                    // alert(response);
+                    $('#view').html(response);
+                    if(response==1){
+                        location.href = "/ajsas/pages/Back_end/home_backend.php";
+                    // sessionStorage.setItem('acc_id', response);
+                }else if(response==2){
+                    location.href = "/ajsas/pages/Front_end/allarticle.php";
+                }else if(response==3){
+                    location.href = "/ajsas/pages/Front_end/readarticle.php";
+                }else if(response==4){
+                    location.href = "/ajsas/pages/Front_end/article_data.php";
+                }else{
+                    
+                    $('#msg').modal('show');
+                    // $('body').addClass('modal-open');
+                }
+            }); 
+                }, 270);
+                return false;
+            }
+
+        });
+    });
+</script>
+
+</body>
+</html>
