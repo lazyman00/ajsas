ee9e2200dd538f035f77f7486b8e67f3953efff4
lazyman00 <sky_of_light@hotmail.com>
2019-11-24 13:36:30 +0700

    B

---
+++ pages/Front_end/allarticle/view_allaricle_data.php
@@ -0,0 +1,49 @@
+<?php  include('../../../connect/connect.php'); ?>
+<?php 
+$sql = sprintf("SELECT * FROM `article` WHERE `article_id` = %s",GetSQLValueString($_POST['article_id'], 'int'));
+$query = $conn->query($sql);
+$row = $query->fetch_assoc();
+?>
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="article_name_th">ชื่อบทความภาษาไทย :</label>
+        <input type="text" readonly="" name="article_name_th" class="form-control" id="article_name_th" value="<?php echo $row['article_name_th']; ?>">
+    </div>
+    <div class="form-group col-md-12">
+        <label for="abstract_th">บทคัดย่อภาษาไทย :</label>
+        <textarea readonly="" name="abstract_th" class="form-control" id="abstract_th" rows="5"><?php echo $row['abstract_th']; ?></textarea>
+    </div>
+</div>
+<div class="form-row">
+    <div class="form-group col-md-12">
+        <label for="article_name_en">ชื่อบทความภาษาอังกฤษ :</label>
+        <input type="text" readonly="" name="article_name_en" class="form-control" id="article_name_en" value="<?php echo $row['article_name_en']; ?>">
+    </div>
+    <div class="form-group col-md-12">
+        <label for="abstract_en">บทคัดย่อภาษาอังกฤษ :</label>
+        <textarea readonly="" name="abstract_en" class="form-control" id="abstract_en" rows="5"><?php echo $row['abstract_en']; ?></textarea>
+    </div>
+</div>
+<div class="form-group">
+    <label for="type_article_name">ประเภทบทความ :</label>
+    <input type="text" readonly="" class="form-control" id="type_article_name">
+    <input type="hidden" name="type_article_name" value="">
+</div>
+<div class="form-group">
+    <label for="keyword_th">คำสำคัญภาษาไทย :</label>
+    <input type="text" readonly="" name="keyword_th" class="form-control" id="keyword_th" value="<?php echo $row['keyword_th']; ?>">
+</div>
+<div class="form-group">
+    <label for="keyword_en">คำสำคัญภาษาอังกฤษ :</label>
+    <input type="text" readonly="keyword_en" name="" class="form-control" id="keyword_en" value="<?php echo $row['keyword_en']; ?>">
+</div>
+<div class="form-group">
+    <label for="attach_article">แบบไฟล์บทความ :</label>
+    <a target="_blank" href="../../files_work/<?php echo $row['attach_article']; ?>"><?php echo $row['attach_article']; ?></a>
+</div>
+<div class="form-row">
+    <div class="form-group col-md-4">
+        <label for="date_article">วันที่ส่งบทความ :</label>
+        <input type="text" readonly="" name="date_article" class="form-control" id="date_article" value="<?php echo $row['date_article']; ?>">
+    </div>
+</div>
