e4422bc0ca43392c2b7a6e78cb2dabd89d9b1645
supasek <nsupasek178@gmail.com>
2019-12-14 12:40:01 +0700

    naka
    แก้ หลังบ้าน

--- pages/Back_end/header.php
+++ pages/Back_end/header.php
@@ -13,8 +13,8 @@
    // $row_data = $query->fetch_assoc();   
 ?>
  <!-- Topbar -->
- <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
+ <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow" style="margin-top: -;top: 0px;height: 96px;">
+ <img src="../../img/banner2-01.png">
 <!-- Sidebar Toggle (Topbar) -->
 <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
   <i class="fa fa-bars"></i>
@@ -61,10 +61,10 @@
         <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
         รีเซ็ตรหัสผ่าน
       </a>
-      <!-- <a class="dropdown-item" href="#">
+      <a class="dropdown-item" href="#">
         <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
         Change Data User
-      </a> -->
+      </a>
       <div class="dropdown-divider"></div>
       <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
         <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
