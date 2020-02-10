91e45c27c21e015d7e2119a198542f2f7eecf673
lazyman00 <sky_of_light@hotmail.com>
2020-02-10 18:39:41 +0700

    1

--- rechack_email/sql.php
+++ rechack_email/sql.php
@@ -32,73 +32,75 @@ $sql = sprintf("UPDATE `tb_sendmail` SET `sta_sendMail`=%s WHERE `id_sendMail`=%
 	GetSQLValueString($cl3,'text'),
 	GetSQLValueString($_POST['sm'],'text'));
 $query = $conn->query($sql);
+
 if(($query)&&($cl3=="1")){
-	$cl4 = "";
-	if($_POST['type_user_id']){
-		$cl4 = $_POST['type_user_id'];
-	}
-	$cl5 = "";
-	if($_POST['add_email']){
-		$cl5 = $_POST['add_email'];
-	}
-	$cl6 = "";
-	if($_POST['add_pass']){
-		$cl6 = $_POST['add_pass'];
-	}
-	$cl7 = "";
-	if($_POST['academe_id']){
-		$cl7 = $_POST['academe_id'];
-	}
-	$cl8 = "";
-	if($_POST['type_title_id']){
-		$cl8 = $_POST['type_title_id'];
-	}
-	$cl9 = "";
-	if($_POST['pre_id']){
-		$cl9 = $_POST['pre_id'];
-	}
-	$cl10 = "";
-	if($_POST['name_th']){
-		$cl10 = $_POST['name_th'];
-	}
-	$cl11 = "";
-	if($_POST['surname_th']){
-		$cl11 = $_POST['surname_th'];
-	}
-	$cl12 = "";
-	if($_POST['name_en']){
-		$cl12 = $_POST['name_en'];
-	}
-	$cl13 = "";
-	if($_POST['surname_en']){
-		$cl13 = $_POST['surname_en'];
-	}
-	$cl14 = "";
-	if($_POST['address_user']){
-		$cl14 = $_POST['address_user'];
-	}
-	$cl15 = "";
-	if($_POST['phonenumber_user']){
-		$cl15 = $_POST['phonenumber_user'];
-	}
+	if(empty($_POST['password'])){
+		$cl4 = "";
+		if($_POST['type_user_id']){
+			$cl4 = $_POST['type_user_id'];
+		}
+		$cl5 = "";
+		if($_POST['add_email']){
+			$cl5 = $_POST['add_email'];
+		}
+		$cl6 = "";
+		if($_POST['add_pass']){
+			$cl6 = $_POST['add_pass'];
+		}
+		$cl7 = "";
+		if($_POST['academe_id']){
+			$cl7 = $_POST['academe_id'];
+		}
+		$cl8 = "";
+		if($_POST['type_title_id']){
+			$cl8 = $_POST['type_title_id'];
+		}
+		$cl9 = "";
+		if($_POST['pre_id']){
+			$cl9 = $_POST['pre_id'];
+		}
+		$cl10 = "";
+		if($_POST['name_th']){
+			$cl10 = $_POST['name_th'];
+		}
+		$cl11 = "";
+		if($_POST['surname_th']){
+			$cl11 = $_POST['surname_th'];
+		}
+		$cl12 = "";
+		if($_POST['name_en']){
+			$cl12 = $_POST['name_en'];
+		}
+		$cl13 = "";
+		if($_POST['surname_en']){
+			$cl13 = $_POST['surname_en'];
+		}
+		$cl14 = "";
+		if($_POST['address_user']){
+			$cl14 = $_POST['address_user'];
+		}
+		$cl15 = "";
+		if($_POST['phonenumber_user']){
+			$cl15 = $_POST['phonenumber_user'];
+		}
 
 
-	$sql = sprintf("UPDATE `user` SET `type_user_id`=%s,`email`=%s,`password`=%s,`academe_id`=%s,`type_title_id`=%s,`pre_id`=%s,`name_th`=%s,`surname_th`=%s,`name_en`=%s,`surname_en`=%s,`address_user`=%s,`phonenumber_user`=%s WHERE `user_id`=%s",
-		GetSQLValueString($cl4,'text'),
-		GetSQLValueString($cl5,'text'),
-		GetSQLValueString(md5($cl6),'text'),
-		GetSQLValueString($cl7,'text'),
-		GetSQLValueString($cl8,'text'),
-		GetSQLValueString($cl9,'text'),
-		GetSQLValueString($cl10,'text'),
-		GetSQLValueString($cl11,'text'),
-		GetSQLValueString($cl12,'text'),
-		GetSQLValueString($cl13,'text'),
-		GetSQLValueString($cl14,'text'),
-		GetSQLValueString($cl15,'text'),
-		GetSQLValueString($cl2,'text'));
-	$query = $conn->query($sql);
-
+		$sql = sprintf("UPDATE `user` SET `type_user_id`=%s,`email`=%s,`password`=%s,`academe_id`=%s,`type_title_id`=%s,`pre_id`=%s,`name_th`=%s,`surname_th`=%s,`name_en`=%s,`surname_en`=%s,`address_user`=%s,`phonenumber_user`=%s WHERE `user_id`=%s",
+			GetSQLValueString($cl4,'text'),
+			GetSQLValueString($cl5,'text'),
+			GetSQLValueString(md5($cl6),'text'),
+			GetSQLValueString($cl7,'text'),
+			GetSQLValueString($cl8,'text'),
+			GetSQLValueString($cl9,'text'),
+			GetSQLValueString($cl10,'text'),
+			GetSQLValueString($cl11,'text'),
+			GetSQLValueString($cl12,'text'),
+			GetSQLValueString($cl13,'text'),
+			GetSQLValueString($cl14,'text'),
+			GetSQLValueString($cl15,'text'),
+			GetSQLValueString($cl2,'text'));
+		$query = $conn->query($sql);
+	}
 
 	$sql = sprintf("UPDATE `article` SET `sta_work`=%s WHERE `article_id`=%s",
 		GetSQLValueString('1','text'),
